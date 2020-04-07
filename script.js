0abe7ffeab1105af893a5e70ed2f608ad4782e24
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-04-07 16:40:28 +0300

    feat: add save results, show results and block movements on stop

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const _DEFAULT_VALUES = {
   puzzleDimension: 4,
   startGameTIme: new Date(),
   steps: 0,
+  gameInProgress: false,
 };
+
+let savedGame = window.localStorage.getItem("savedGame");
+if (savedGame) {
+  _DEFAULT_VALUES.gameInProgress = true;
+  savedGame = JSON.parse(savedGame);
+  _DEFAULT_VALUES.puzzleDimension = savedGame.dimension;
+  _DEFAULT_VALUES.steps = savedGame.steps;
+  _DEFAULT_VALUES.startGameTIme = new Date() - savedGame.time;
+}
 
 let body = document.querySelector("body");
 
@@ -42,6 +52,8 @@ saveButton.classList.add(
 saveButton.innerText = "Сохранить";
 buttons.appendChild(saveButton);
 
+saveButton.addEventListener("click", saveGame);
+
 let resultButton = document.createElement("button");
 resultButton.classList.add(
   "game-area__buttons__button",
@@ -49,6 +61,8 @@ resultButton.classList.add(
 );
 resultButton.innerText = "Результаты";
 buttons.appendChild(resultButton);
+
+resultButton.addEventListener("click", showResults);
 
 let timer = document.createElement("div");
 timer.className = "game-area__timer";
@@ -71,8 +85,12 @@ timer.appendChild(time);
 
 let currentTime = document.createElement("div");
 currentTime.className = "game-area__timer__inner";
-currentTime.innerText = new Date() - _DEFAULT_VALUES.startGameTIme;
+currentTime.innerText = msToTime(new Date() - _DEFAULT_VALUES.startGameTIme);
 time.appendChild(currentTime);
+
+if (savedGame) {
+  startTimer();
+}
 
 let puzzle = document.createElement("div");
 puzzle.classList.add(
@@ -100,17 +118,13 @@ for (let i = 3; i <= 8; i++) {
   specificSize.innerText = `${i}x${i}`;
   specificSize.addEventListener("click", () => {
     _DEFAULT_VALUES.puzzleDimension = i;
-    let cellsArr = puzzle.querySelectorAll(".game-area__puzzle__puzzle-cell");
-    cellsArr.forEach((cell) => {
-      cell.remove();
-    });
-    drawGrid(i);
-    // shuffle(i);
+    stopGame();
   });
   dimensions.appendChild(specificSize);
 }
 
 drawGrid(_DEFAULT_VALUES.puzzleDimension);
+if (savedGame) shuffle(_DEFAULT_VALUES.puzzleDimension, savedGame.grid);
 
 function drawGrid(dimension) {
   let cellValue = dimension * dimension;
@@ -130,6 +144,7 @@ function drawGrid(dimension) {
       let move;
 
       puzzleCell.addEventListener("click", (e) => {
+        if (!_DEFAULT_VALUES.gameInProgress) return;
         move = getMove(puzzleCell, dimension);
         if (!move) return;
         if (animationEnd) {
@@ -147,6 +162,19 @@ function drawGrid(dimension) {
         stepsCount.innerText = _DEFAULT_VALUES.steps;
 
         animationEnd = true;
+
+        if (isWin()) {
+          let elapsedTime = msToTime(
+            new Date() - _DEFAULT_VALUES.startGameTIme
+          );
+          setTimeout(() => {
+            stopGame();
+            window.alert(
+              `Ура! Вы решили головоломку за ${elapsedTime} и ${_DEFAULT_VALUES.steps} ходов`
+            );
+          }, 0);
+          saveResult(elapsedTime, _DEFAULT_VALUES.steps);
+        }
       });
     }
     puzzle.appendChild(puzzleCell);
@@ -156,13 +184,61 @@ function drawGrid(dimension) {
   puzzle.classList.add("game-area__puzzle", `game-area__puzzle_${dimension}`);
 }
 
-function shuffle(dimension) {
+function showResults() {
+  let results = [];
+  let sResults = window.localStorage.getItem("result");
+  if (sResults) {
+    results = JSON.parse(sResults);
+  }
+  let i = 1;
+  resultString = "Последние результаты:\n#     Ходы     Время\n";
+  for (let el of results) {
+    resultString += `${i})     ${el.steps}    ${el.elapsedTime}\n`;
+    i++;
+  }
+  window.alert(resultString);
+}
+
+function saveResult(elapsedTime, steps) {
+  let results = [];
+  let sResults = window.localStorage.getItem("result");
+  if (sResults) {
+    results = JSON.parse(sResults);
+  }
+  if (results.length < 10) results.push({ elapsedTime, steps });
+  else {
+    if (results[9].steps > steps) {
+      results.pop();
+      results.push({ elapsedTime, steps });
+    } else {
+      if (results[9].elapsedTime > elapsedTime && results[9].steps == steps) {
+        results.pop();
+        results.push({ elapsedTime, steps });
+      }
+    }
+  }
+  results.sort((a, b) => {
+    if (a.steps == b.steps) {
+      if (a.elapsedTime.length == b.elapsedTime.length) {
+        if (a.elapsedTime >= b.elapsedTime) return 1;
+        else return -1;
+      } else return a.elapsedTime.length - b.elapsedTime.length;
+    } else return a.steps - b.steps;
+  });
+  window.localStorage.setItem("result", JSON.stringify(results));
+}
+
+function shuffle(dimension, sortOrder) {
   let puzzle = document.querySelector(
     `body > div > div.game-area__puzzle_${dimension}`
   );
   let cellsArr = puzzle.querySelectorAll(".game-area__puzzle__puzzle-cell");
   cellsArr = [...cellsArr];
-  cellsArr.sort(() => Math.random() - 0.5);
+  if (sortOrder) {
+    cellsArr = sortOrder.map((e) => cellsArr[parseInt(e) - 1]);
+  } else {
+    cellsArr.sort(() => Math.random() - 0.5);
+  }
   cellsArr.forEach((cell) => {
     cell.remove();
     puzzle.appendChild(cell);
@@ -170,15 +246,22 @@ function shuffle(dimension) {
 }
 
 function startGame() {
+  _DEFAULT_VALUES.gameInProgress = true;
   shuffle(_DEFAULT_VALUES.puzzleDimension);
   _DEFAULT_VALUES.steps = 0;
+  stepsCount.innerText = _DEFAULT_VALUES.steps;
   _DEFAULT_VALUES.startGameTIme = new Date();
   currentTime.innerText = msToTime(new Date() - _DEFAULT_VALUES.startGameTIme);
+  startTimer();
+}
+
+function startTimer() {
   setInterval(() => {
     currentTime.innerText = msToTime(
       new Date() - _DEFAULT_VALUES.startGameTIme
     );
   }, 1000);
+
   timer.classList.add("game-area__timer_visible");
 }
 
@@ -189,6 +272,30 @@ function stopGame() {
   });
   drawGrid(_DEFAULT_VALUES.puzzleDimension);
   timer.classList.remove("game-area__timer_visible");
+  _DEFAULT_VALUES.gameInProgress = false;
+}
+
+function saveGame() {
+  let savedGame = {};
+  savedGame.steps = _DEFAULT_VALUES.steps;
+  savedGame.time = new Date() - _DEFAULT_VALUES.startGameTIme;
+  savedGame.dimension = _DEFAULT_VALUES.puzzleDimension;
+
+  let currentGameGrid = puzzle.querySelectorAll(
+    ".game-area__puzzle__puzzle-cell"
+  );
+  currentGameGrid = [...currentGameGrid];
+  savedGame.grid = currentGameGrid.map((e) => e.getAttribute("key"));
+  window.localStorage.setItem("savedGame", JSON.stringify(savedGame));
+}
+
+function isWin() {
+  let cellsArr = puzzle.querySelectorAll(".game-area__puzzle__puzzle-cell");
+  cellsArr = [...cellsArr];
+  for (let i = 0; i < cellsArr.length; i++) {
+    if (cellsArr[i].getAttribute("key") != i + 1) return false;
+  }
+  return true;
 }
 
 function getCordsByPosition(position, dim) {
